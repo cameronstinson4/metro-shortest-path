ef1267903b281191f179a49f38da1e0d5607a39a
bjm808 <cameron.stinson@capitalone.com>
2017-09-12 16:00:12 -0400

    updated readme with pages link

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
-//const stations = require("../data/stations")
-
-// create an array with nodes
-// var nodes = new vis.DataSet([
-//   {id: 1, label: 'Node 1'},
-//   {id: 2, label: 'Node 2'},
-//   {id: 3, label: 'Node 3'},
-//   {id: 4, label: 'Node 4'},
-//   {id: 5, label: 'Node 5'}
-// ]);
 
 var dataSet = [];
 var edges = [];
@@ -17,12 +7,15 @@ stations.forEach(function(station) {
     id: station.Code,
     label: `${station.Name}`,
     color: parseColor(station.LineCode1),
-    shape: "box"
+    shape: "box",
   });
   if (station.StationTogether1 !== "") {
     edges.push({
       from: station.Code,
-      to: station.StationTogether1
+      to: station.StationTogether1,
+      color: "black",
+      length: 0,
+      label: "Transfer"
     });
   }
 }, this);
