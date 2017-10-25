42dd7f161246f34669167110122412badea13277
bjm808 <cameron.stinson@capitalone.com>
2017-10-25 10:43:09 -0400

    added shortest path ability

--- js/main.js
+++ js/main.js
@@ -1,28 +1,44 @@
-
-var dataSet = [];
+var nodes = [];
 var edges = [];
 
-stations.forEach(function(station) {
-  dataSet.push({
+stations.forEach(function (station) {
+  var node = {
     id: station.Code,
     label: `${station.Name}`,
     color: parseColor(station.LineCode1),
-    shape: "box",
-  });
+    shape: "square",
+    mass: 10,
+    size: 50,
+    title: station.Code,
+    font: {size: 50, strokeColor: '#ffffff', background: 'white'	}
+  };
+  if (node.id === "A01") {
+    node.fixed = {
+      x: true,
+      y: true
+    }
+  };
+  nodes.push(node);
   if (station.StationTogether1 !== "") {
     edges.push({
+      id: station.Code + " " + station.StationTogether1 + " " + null,      
       from: station.Code,
       to: station.StationTogether1,
       color: "black",
-      length: 0,
-      label: "Transfer"
+      length: 100,
+      label: "Transfer",
+      width: 25
     });
   }
 }, this);
 
 edges.push(...addLineEdges());
 
-var nodes = new vis.DataSet(dataSet);
+//Algorithm stuff
+var graph = new Graph();
+addVerticesToGraph(nodes, edges);
+
+var nodes = new vis.DataSet(nodes);
 
 // create an array with edges
 var edges = new vis.DataSet(edges);
@@ -35,16 +51,17 @@ var data = {
   nodes: nodes,
   edges: edges
 };
-var options = {  
-  physics:{
+
+var options = {
+  physics: {
     enabled: true,
     barnesHut: {
       gravitationalConstant: -10000,
-      centralGravity: 0,
+      centralGravity: -0.5,
       springLength: 95,
       springConstant: 0.04,
       damping: 0.09,
-      avoidOverlap: 0
+      avoidOverlap: .5
     },
     maxVelocity: 1000,
     minVelocity: 0.1,
@@ -64,11 +81,17 @@ var options = {
 // initialize your network!
 var network = new vis.Network(container, data, options);
 
+document.getElementById("shortestPathForm").addEventListener("submit", (event) => {
+  event.preventDefault();
+  spa(document.getElementById("start").value, document.getElementById("end").value);
+}, false);
+
+
 function parseColor(code) {
-  switch(code) {
+  switch (code) {
     case "RD":
       return "red";
-    case "BL": 
+    case "BL":
       return "blue";
     case "YL":
       return "yellow";
@@ -79,7 +102,7 @@ function parseColor(code) {
     case "SV":
       return "silver";
 
-    default: 
+    default:
       return "pink";
   }
 }
@@ -88,14 +111,16 @@ function dry(array) {
   var edges = [];
   var previous = null;
 
-  array.forEach(function(station) {
+  array.forEach(function (station) {
     if (previous !== null) {
       edges.push({
+        id: station.StationCode + " " + previous.StationCode + " " + parseColor(station.LineCode),
         from: station.StationCode,
         to: previous.StationCode,
         color: parseColor(station.LineCode),
-        length: station.DistanceToPrev/10,
-        label: station.DistanceToPrev
+        length: station.DistanceToPrev / 10,
+        label: station.DistanceToPrev,
+        width: 25
       })
     }
     previous = station;
@@ -105,7 +130,7 @@ function dry(array) {
 }
 
 function addLineEdges() {
-  
+
   var edges = [];
 
   edges.push(...dry(blPath));
@@ -116,4 +141,58 @@ function addLineEdges() {
   edges.push(...dry(ylPath));
 
   return edges;
+
+}
+
+function addVerticesToGraph(nodes, edges) {
+  var vertices = {};
+
+  nodes.forEach((node) => {
+    vertices[node.id] = {};
+  });
+
+  edges.forEach((edge) => {
+
+    vertices[edge.from][edge.to] = edge.length;
+    vertices[edge.to][edge.from] = edge.length;
+
+  });
+
+  for (vertex in vertices) {
+    graph.addVertex(vertex, vertices[vertex]);
+  }
+}
+
+function showResult(nodes) {
+  var nodeToSelect = nodes.map((node) => {
+    if (node.id === str) {
+      return node;
+    }
+  })
+  return nodeToSelect;
+}
+
+function spa(start, end) {
+
+  var path = graph.shortestPath(start, end, network);
+
+  //network.setSelection({ nodes: path });
+
+  for (var i = 0; i < path.length; i++) {
+    nodes._data[path[i]].color = "black";
+    nodes._data[path[i]].size =  100;
+    nodes._data[path[i]].font =  {size: 100, strokeColor: '#ffffff', background: 'white'	};
+    
+    if (i !== 0 && i !== path.length) {
+      for (id in edges._data) {
+        if (id.substring(0, 7) == `${path[i]} ${path[i - 1]}` || id.substring(0, 7) == `${path[i]} ${path[i + 1]}`) {
+          edges._data[id].color = "black";
+          edges._data[id].width = 100;
+          
+        }
+      }
+    }
+  }
+
+  network.setData({nodes: nodes, edges: edges});
 }
